4a1297fcbb24cd13cf538ea1c9a0120b8806541f
Ticktock <smu@ticktock.local>
2018-03-29 00:02:01 +0800

    halfway done

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const { Client } = require('pg');
 
 // Initialise postgres client
 const client = new Client({
-  user: 'akira',
+  user: 'smu',
   host: '127.0.0.1',
   database: 'pokemons',
   port: 5432,
 });
+
 
 /**
  * ===================================
@@ -36,23 +37,111 @@ app.set('view engine', 'handlebars');
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
+  let values = [];
+  let context = "";
+  let newPokemon = [];
 
-  // respond with HTML page displaying all pokemon
-});
+  /////////////Query database for all pokemon///////////////
 
 app.get('/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
   response.render('new');
 });
 
+app.get('/', (request, response) => {
+  
+  const client = new Client({
+    user: 'smu',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432,
+  });
+
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
+    let queryString = 'SELECT * FROM pokemon';
+    client.query(queryString, (err, res) => {
+
+      if (err) {
+        console.error('query error:', err.stack);
+      } else
+      {
+        for (i=0; i<res.rows.length; i++){
+          newPokemon.push(res.rows[i]);
+        };
+      };
+      // respond with HTML page displaying all pokemon
+      // redirect to home page
+      response.render("home", {pokemon: newPokemon});
+      client.end();
+    })
+  });
+});
+
+
+//GET request to return HTML data about pokemon ID 2
+
+app.get('/:id', (request, response) => {
+
+    let client = new Client({
+    user: 'smu',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432,
+    });
+
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
+    let id = request.params.id;
+
+    let queryString = 'SELECT * FROM pokemon WHERE id = $1;';
+    let value = [id];
+
+    client.query(queryString, value, (err, res) => {
+      if (err) {
+        console.error('query error:', err.stack);
+      } else 
+      { 
+        response.render("pokemon", {pokemon: res.rows[0]});
+        client.end();
+      };
+    })
+  })
+})
+
+app.get('/:id/edit', (request, response) => {
+
+  let client = new Client({
+    user: 'smu',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432,
+  });
+
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
+    let id = request.params.id;
+
+    let queryString = 'SELECT * FROM pokemon WHERE id = $1;';
+    let value = [id];
+
+    client.query(queryString, value, (err, res) => {
+      if (err) {
+        console.error('query error:', err.stack);
+      } else 
+      {
+        response.render("edit", {pokemon: res.rows[0]});
+        client.end();
+      };
+    });
+  });
+});
 
 app.post('/pokemon', (req, response) => {
   let params = req.body;
 
-  const queryString = 'INSERT INTO pokemon(name, height) VALUES($1, $2)'
-  const values = [params.name, params.height];
+  let queryString = 'INSERT INTO pokemon(name, height) VALUES($1, $2);'
+  let values = [params.name, params.height];
 
   client.connect((err) => {
     if (err) console.error('connection error:', err.stack);
@@ -71,6 +160,34 @@ app.post('/pokemon', (req, response) => {
   });
 });
 
+//Save the created pokemon into database
+
+app.post("/", (request, response) => {
+
+  let params = request.body;
+  console.log(params);
+  
+  let queryString = 'INSERT INTO pokemon (num, name, img, height, weight) VALUES ($1, $2, $3, $4, $5);';
+  let values = [params.num, params.name, params.img, params.height, params.height];
+
+  let client = new Client({
+    user: 'smu',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432,
+  });
+
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
+
+    client.query(queryString, values, (err, res) => {
+      response.redirect("/");
+      client.end();
+    });
+  });
+});
+
+app.post()
 
 /**
  * ===================================
@@ -78,3 +195,6 @@ app.post('/pokemon', (req, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
